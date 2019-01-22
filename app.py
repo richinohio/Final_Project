74b2dd244052ec56d17916c8c4323e33c14e0b07
Bryce Basista <basista.bryce@gmail.com>
2019-01-22 13:13:49 -0500

    test app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, session, g, redirect, url_for, abort, \
      render_template, flash
 import os
-from predict import prediction_log, prediction_lin
+from predict import prediction_lin
 
 app1 = Flask(__name__)
 
@@ -15,12 +15,13 @@ def print():
     att2 = request.form['BedRooms']
     att3 = request.form['Bathroom']
     att4 = request.form['SQFT']
+    
+    #predictive function using model.prediction
+    rent_pred = prediction_lin(att1,att2,att3,att4)
+    return render_template('result_lin.html', result=rent_pred)
 
-#predictive function using model.prediction
-rent_pred = prediction_lin(att1,att2,att3,att4)
 
-return render_template('result_lin.html', result=rent_pred)
-
+"""
 app2 = Flask(__name__)
 
 @app2.route('/', methods=['GET'])
@@ -34,15 +35,11 @@ def print():
     att3 = request.form['Bathroom']
     att4 = request.form['SQFT']
     att5 = request.form['chrent']
-
-#predictive function using model.prediction
-vacancy_pred = prediction_log(att1,att2,att3,att4,att5) 
-
-return render_template('result_log.html', result=LOS_pred)
+    
+    #predictive function using model.prediction
+    vacancy_pred = prediction_log(att1,att2,att3,att4,att5)
+    return render_template('result_log.html', result=LOS_pred)
+"""
 
 if __name__ == '__main__':
-    app1.run(port=5000, debug=True)
-    app2.run(port=8000, debug=True)
-
-
-
+    app1.run(port=5000, debug=True)   
--- predict.py
+++ predict.py
@@ -1,4 +1,12 @@
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+from keras.utils import to_categorical
+from keras.models import Sequential
+from keras.layers import Dense
 import matplotlib.pyplot as plt
 import pandas as pd
 import os
@@ -7,6 +15,7 @@ warnings.simplefilter('ignore')
 import numpy as np
 import pickle
 
+
 def prediction_lin(Property_Code, BedRooms, Bathroom,SQFT):
     
     # Create of row of data that comabines all user inputs
@@ -14,13 +23,14 @@ def prediction_lin(Property_Code, BedRooms, Bathroom,SQFT):
     test_lin = pd.DataFrame(title_lin)
 
     #import pickled model
-    trained_model_lin = joblib.load('model.pkl')
+    trained_model_lin = pickle.load(open('models/linear_model.pkl','rb'))
 
     # Make prediction from the loaded random forest model
     predict_lin = trained_model_lin.predict(test_lin)[0]
     result_lin="${:,.2f}".format(predict_lin)
     return result_lin
 
+"""
 def prediction_log(Property_Code, BedRooms, Bathroom,SQFT, chrent):
     
     # Create of row of data that comabines all user inputs
@@ -28,9 +38,10 @@ def prediction_log(Property_Code, BedRooms, Bathroom,SQFT, chrent):
     test_log = pd.DataFrame(title_log)
 
     #import pickled model
-    trained_model_log = joblib.load('model.pkl')
+    trained_model_log = pickle.load(open('logistic_deep_model.pkl','rb'))
 
     # Make prediction from the loaded random forest model
     predict_log = trained_model_log.predict(test_log)[0]
     result_log="${:,.2f}".format(predict_log)
     return result_log
+"""
