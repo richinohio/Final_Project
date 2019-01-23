980ca0aba02fb4d2b9f0196719ac4e40a5cd0211
Rocky Fur <danny.11.furman@gmail.com>
2019-01-22 20:57:02 -0500

    Messing with flask

---
+++ New folder/app.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request, session, g, redirect, url_for, abort, \
+     render_template, flash
+import os
+from predict import prediction_lin
+
+app1 = Flask(__name__)
+
+@app1.route('/', methods=['GET'])
+def mainpage():
+    return render_template('index_lin.html')
+
+@app1.route('/',methods=['POST'])
+def print():
+    att1 = request.form['Property_Code']
+    att2 = request.form['Bedrooms']
+    att3 = request.form['Bathrooms']
+    att4 = request.form['SQFT']
+    
+    #predictive function using model.prediction
+    rent_pred = prediction_lin(att1,att2,att3,att4)
+    return render_template('result_lin.html', result=rent_pred)
+
+
+"""
+app2 = Flask(__name__)
+
+@app2.route('/', methods=['GET'])
+def mainpage():
+    return render_template('index_log.html')
+
+@app2.route('/',methods=['POST'])
+def print():
+    att1 = request.form['Property_Code']
+    att2 = request.form['BedRooms']
+    att3 = request.form['Bathroom']
+    att4 = request.form['SQFT']
+    att5 = request.form['chrent']
+    
+    #predictive function using model.prediction
+    vacancy_pred = prediction_log(att1,att2,att3,att4,att5)
+    return render_template('result_log.html', result=LOS_pred)
+"""
+
+if __name__ == '__main__':
+    app1.run(port=5000, debug=True)   
---
+++ New folder/predict.py
@@ -0,0 +1,47 @@
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+from keras.utils import to_categorical
+from keras.models import Sequential
+from keras.layers import Dense
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+import warnings
+warnings.simplefilter('ignore')
+import numpy as np
+import pickle
+
+
+#def prediction_lin(Property_Code, Bedrooms, Bathrooms,SQFT):
+def prediction_lin(Property_Code, Bedrooms, Bathrooms,SQFT):
+    # Create of row of data that comabines all user inputs
+    #title_lin={"Property_Code":[Property_Code], "Bedrooms":[Bedrooms], "Bathrooms":[Bathrooms],"SQFT":[SQFT]}
+    #test_lin = pd.DataFrame(title_lin)
+
+    #import pickled model
+    trained_model_lin = pickle.load(open('models/linear_model.pkl','rb'))
+
+    # Make prediction from the loaded random forest model
+    predict_lin = trained_model_lin.predict([[propertycode[0],propertycode[1],0,1,1,2.5,1000]])
+    #result_lin="${:,.2f}".format(predict_lin)
+    return predict_lin
+
+"""
+def prediction_log(Property_Code, BedRooms, Bathroom,SQFT, chrent):
+    
+    # Create of row of data that comabines all user inputs
+    title_log={"Property_Code":[Property_Code], "BedRooms":[BedRooms], "Bathroom":[Bathroom],"SQFT":[SQFT],"chrent":[chrent]}
+    test_log = pd.DataFrame(title_log)
+
+    #import pickled model
+    trained_model_log = pickle.load(open('logistic_deep_model.pkl','rb'))
+
+    # Make prediction from the loaded random forest model
+    predict_log = trained_model_log.predict(test_log)[0]
+    result_log="${:,.2f}".format(predict_log)
+    return result_log
+"""
