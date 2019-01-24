bc99b18a36530c90f297bae32c8e3d9912a35279
Bryce Basista <basista.bryce@gmail.com>
2019-01-23 20:16:08 -0500

    logistic model html and flask app

--- predict.py
+++ predict.py
@@ -7,6 +7,7 @@ from sklearn.metrics import mean_squared_error
 from keras.utils import to_categorical
 from keras.models import Sequential
 from keras.layers import Dense
+from keras.backend import clear_session
 import matplotlib.pyplot as plt
 import pandas as pd
 import os
@@ -18,26 +19,22 @@ import pickle
 
 def prediction_lin(Property_Code1, p2,p3,p4, Bedrooms, Bathrooms,SQFT):
 
-
     #import pickled model
     trained_model_lin = pickle.load(open('models/linear_model.pkl','rb'))
 
-    # Make prediction from the loaded random forest model
+    # Make prediction from the loaded model
     predict_lin = trained_model_lin.predict([[Property_Code1,p2,p3,p4,Bedrooms,Bathrooms,SQFT]])
     #result_lin="${:,.2f}".format(predict_lin)
     return predict_lin
 
 
-def prediction_log(Property_Code, BedRooms, Bathroom,SQFT, chrent):
+def prediction_log(Property_Code1, p2,p3,p4, Bedrooms, Bathrooms,SQFT,Rent):
     
-    # Create of row of data that comabines all user inputs
-    title_log={"Property_Code":[Property_Code], "BedRooms":[BedRooms], "Bathroom":[Bathroom],"SQFT":[SQFT],"chrent":[chrent]}
-    test_log = pd.DataFrame(title_log)
-
     #import pickled model
-    trained_model_log = pickle.load(open('logistic_deep_model.pkl','rb'))
+    list_pickle = open('models/logisitic_depp_network.pkl','rb') 
+    trained_model_log = pickle.load(list_pickle)
 
-    # Make prediction from the loaded random forest model
-    predict_log = trained_model_log.predict(test_log)[0]
-    result_log="${:,.2f}".format(predict_log)
-    return result_log
+# Make prediction from the loaded model
+    predict_log = trained_model_log.predict_classes([[[SQFT,Bedrooms,Bathrooms,Rent,Property_Code1, p2,p3,p4]]])
+    clear_session()
+    return predict_log
